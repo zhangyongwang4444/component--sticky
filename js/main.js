8a75ce4a4a971ff10441fb15c91311d4f87d7cac
zhangyongwang <349368992@qq.com>
2018-09-30 22:54:58 +0800

    Sticky组件

--- js/main.js
+++ js/main.js
@@ -1,26 +1,52 @@
-// class Sticky {
-//     constructor(selector, n) {
-//         this.elements = $(selector)
-//         this.offset = n
-//     }
-// }
+class Sticky {
+    constructor(selector, n) {
+        this.elements = $(selector)
+        this.offset = n || 0
+        this.cacheOffsets()
+        this.listenToScroll()
+    }
+    cacheOffsets() {
+        this.offsets = []
+        this.elements.each((index, element) => {
+            this.offsets[index] = $(element).offset()
+        })
+    }
+
+    listenToScroll() {
+        $(window).on('scroll', () => {
+            var scrollY = window.scrollY
+            this.elements.each((index, element) => {
+                var $element = $(element)
+                if (scrollY + this.offset > this.offsets[index].top) {
+                    $element.addClass('sticky')
+                } else {
+                    $element.removeClass('sticky')
+                }
+            })
+        })
+    }
+}
+
+new Sticky('#topbar')
+new Sticky('button', 60)
 
 
 // var sticky = new Sticky(selector, n)
 
-var buttonOffset = $('button').offset() //获取的[第一个元素]的[当前坐标]，[坐标相对于文档]
+// var topbarOffset = $('#topbar').offset()
+// var buttonOffset = $('button').offset() //获取的[第一个元素]的[当前坐标]，[坐标相对于文档]
 
-$(window).on('scroll', function () {
-    var scrollY = window.scrollY  //是[文档]从[顶部]开始滚动过的像素值
-    if (scrollY > 0) {
-        $('#topbar').addClass('sticky')
-    } else {
-        $('#topbar').removeClass('sticky')
-    }
-    if (scrollY + 60 > buttonOffset.top) {
-        $('button').addClass('sticky')
-    } else {
-        $('button').removeClass('sticky')
-    }
-})
+// $(window).on('scroll', function () {
+//     var scrollY = window.scrollY  //是[文档]从[顶部]开始滚动过的像素值
+//     if (scrollY + 0 > topbarOffset.top) {
+//         $('#topbar').addClass('sticky')
+//     } else {
+//         $('#topbar').removeClass('sticky')
+//     }
+//     if (scrollY + 60 > buttonOffset.top) {
+//         $('button').addClass('sticky')
+//     } else {
+//         $('button').removeClass('sticky')
+//     }
+// })
 
